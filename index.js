aa7c0c0db76a285f1eb64c0782dc8ea72ca8c118
pavellll002 <53902665+pavellll002@users.noreply.github.com>
2019-09-20 20:02:43 +0300

    try socket protect

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const Io 		= require('socket.io')(Server)
 
 const defaultOpts = {
   origin: ['https://clucker.ru/chat'],
-  secure: true,
+  secure: false,
   xdomain: false,
   debug: false,
   ipLimit: 0,
