894eb8e18095d3ab6a84f131b698ffba7cbe4ef5
pavellll002 <53902665+pavellll002@users.noreply.github.com>
2019-11-16 12:40:30 +0300

    expires cookie

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ let fileStoreOptions 	= {}
     	saveUninitialized: false,
     	httpOnly: true,
     	cookie:{
-    		maxAge: 1000*60*60*24*365,
+    		expires: new Date(Date.now() + 1000*60*60*24*365),
     	}
 	})) 
 	
